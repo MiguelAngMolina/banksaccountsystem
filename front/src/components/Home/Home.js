0214331467815221988a6ab4c3f98867fc9f815c
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-04-18 17:22:56 -0500

    changes home

--- front/src/components/Home/Home.js
+++ front/src/components/Home/Home.js
@@ -2,19 +2,23 @@ import React from 'react';
 import "./home.css";
 import { useNavigate } from "react-router-dom";
 
-function Home(props) {
-    const navigate = useNavigate();
 
+function Home(props) {
+
+
+    const navigate = useNavigate();
     return (
         <div className="homepage">
-            <div className="card">
-                <h1>Bienvenido!</h1>
-                <p>Sistema de cuentas bancarias</p>
-                <button className='button' onClick={() => navigate("/login")}>
-                    Login
-                </button>
-            </div>
-        </div>
+      <div className='homeprincipal'>
+      <h1>¿Aún no eres parte de la tripulación?</h1>
+      <p>Sistema de cuentas bancarias</p>
+      </div>
+      <button  type="button" className="buttonHome"
+        onClick={() => {
+          navigate("/login");
+        }}
+      >Acceso al inicio de sesión</button>
+    </div>
     );
 }
 
